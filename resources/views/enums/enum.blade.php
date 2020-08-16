d50ee996810f23a39d24a9fa38fa16e17a53db26
Tim Haak <tim@haak.co>
2020-08-16 17:31:31 +0200

    Also quote id in case using uuid

--- resources/views/enums/enum.blade.php
+++ resources/views/enums/enum.blade.php
@@ -3,7 +3,7 @@ namespace App\Models\Enum;
 class {{ $className }}
 {
 @foreach ($enumDataRows as $enumDataRow)
-    public const {{ $enumDataRow->nameString }}_ID = {{ $enumDataRow->id }};
+    public const {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';
 @if ($tableOptions['uuid'])
     public const {{ $enumDataRow->nameString }}_ENUM = '{{ $enumDataRow->uuid }}';
 @endif

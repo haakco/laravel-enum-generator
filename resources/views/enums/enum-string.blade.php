b678a688c915722484913a66212353772dedd7f1
Tim Haak <tim@haak.co>
2023-02-12 16:26:39 +0200

    Added sting id for enum to work around to_string. Need beter answer as some point

--- resources/views/enums/enum-string.blade.php
+++ resources/views/enums/enum-string.blade.php
@@ -5,5 +5,6 @@ enum {{ $className }}: string
 {
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = '{{ $enumDataRow->name }}';
+    case {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';
 @endforeach
 }

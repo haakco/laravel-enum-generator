b5bb5510ed188413026157b589172d1d171dcf7e
Tim Haak <tim@haak.co>
2023-02-12 16:50:10 +0200

    Cleanup

--- resources/views/enums/enum-id.blade.php
+++ resources/views/enums/enum-id.blade.php
@@ -6,6 +6,7 @@ use ArchTech\Enums\InvokableCases;
 enum {{ $className }}: int
 {
     use InvokableCases;
+
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = {{ $enumDataRow->id }};
 @endforeach
--- resources/views/enums/enum-string.blade.php
+++ resources/views/enums/enum-string.blade.php
@@ -6,6 +6,7 @@ use ArchTech\Enums\InvokableCases;
 enum {{ $className }}: string
 {
     use InvokableCases;
+
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = '{{ $enumDataRow->name }}';
     case {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';

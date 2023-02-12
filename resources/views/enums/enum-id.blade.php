cf2918b0a00f49ec3782cc12567ebd83e7a782df
Tim Haak <tim@haak.co>
2023-02-12 16:39:21 +0200

    Helpers to get value for enum

--- resources/views/enums/enum-id.blade.php
+++ resources/views/enums/enum-id.blade.php
@@ -1,8 +1,11 @@
 
 namespace {{ $nameSpace }};
 
+use ArchTech\Enums\InvokableCases;
+
 enum {{ $className }}: int
 {
+    use InvokableCases;
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = {{ $enumDataRow->id }};
 @endforeach
--- resources/views/enums/enum-string.blade.php
+++ resources/views/enums/enum-string.blade.php
@@ -3,6 +3,7 @@ namespace {{ $nameSpace }};
 
 enum {{ $className }}: string
 {
+    use InvokableCases;
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = '{{ $enumDataRow->name }}';
     case {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';

df9580ce76f4b132b0c3f87a6e973720a7051c0b
Tim Haak <tim@haak.co>
2023-02-12 17:30:32 +0200

    Nicer formating

--- resources/views/enums/enum-string.blade.php
+++ resources/views/enums/enum-string.blade.php
@@ -8,7 +8,8 @@ enum {{ $className }}: string
     use InvokableCases;
 
 @foreach ($enumDataRows as $enumDataRow)
-    case {{ $enumDataRow->nameString }} = '{{ $enumDataRow->name }}';
     case {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';
+    case {{ $enumDataRow->nameString }} = '{{ $enumDataRow->name }}';
+
 @endforeach
 }

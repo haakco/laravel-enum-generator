ecac1788ad140727a93beef83a9d733bdbeb79a4
Tim Haak <tim@haak.co>
2024-01-03 20:42:26 +0200

    Add more helpers

--- resources/views/enums/enum-id.blade.php
+++ resources/views/enums/enum-id.blade.php
@@ -1,11 +1,17 @@
 
 namespace {{ $nameSpace }};
 
+use ArchTech\Enums\From;
 use ArchTech\Enums\InvokableCases;
+use ArchTech\Enums\Options;
+use ArchTech\Enums\Values;
 
 enum {{ $className }}: int
 {
+    use From;
     use InvokableCases;
+    use Options;
+    use Values;
 
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }} = {{ $enumDataRow->id }};
--- resources/views/enums/enum-string.blade.php
+++ resources/views/enums/enum-string.blade.php
@@ -1,11 +1,17 @@
 
 namespace {{ $nameSpace }};
 
+use ArchTech\Enums\From;
 use ArchTech\Enums\InvokableCases;
+use ArchTech\Enums\Options;
+use ArchTech\Enums\Values;
 
 enum {{ $className }}: string
 {
+    use From;
     use InvokableCases;
+    use Options;
+    use Values;
 
 @foreach ($enumDataRows as $enumDataRow)
     case {{ $enumDataRow->nameString }}_ID = '{{ $enumDataRow->id }}';
